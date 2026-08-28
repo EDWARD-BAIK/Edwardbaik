f846968a1c61972a0dd95bf0eac15eb69fe41074
EDWARD-BAIK <e87617209@gmail.com>
2026-08-28 15:20:21 +0700

    Update news.js

--- api/news.js
+++ api/news.js
@@ -3,13 +3,8 @@
 export default async function handler(req, res) {
     const { category = 'top' } = req.query;
 
-    // Membaca API Key yang sudah disave di Vercel
-    const API_KEY = process.env.NEWSDATA_API_KEY;
-
-    if (!API_KEY) {
-        return res.status(500).json({ status: 'error', message: 'API Key belum diset di Vercel.' });
-    }
-
+    // API Key langsung dimasukkan di sisi backend
+    const API_KEY = 'pub_beae432af9b04f2ea77803f34d6634e2';
     const BASE_URL = 'https://newsdata.io/api/1/latest';
     const url = `${BASE_URL}?apikey=${API_KEY}&language=id&category=${category}`;
 
@@ -17,6 +12,7 @@ export default async function handler(req, res) {
         const response = await fetch(url);
         const data = await response.json();
 
+        // Cross-Origin Header
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
 
